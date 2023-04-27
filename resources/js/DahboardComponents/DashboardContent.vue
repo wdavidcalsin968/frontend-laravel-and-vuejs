d185e4e4092a3decead66c59f76d000cefc45b9d
Yury B. Huaquilla T <yhuaquillat@est.unap.edu.pe>
2023-04-27 15:07:08 -0500

    tercer commit:(yury) flex

--- resources/js/DahboardComponents/DashboardContent.vue
+++ resources/js/DahboardComponents/DashboardContent.vue
@@ -6,21 +6,31 @@ import Welcome from "@/Components/Welcome.vue";
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+
+
                 <div class="p-2">
-                    <div class="flex row">
-                        <div>
-                            <img src="" alt="">
-                        </div>
-                        <div class="p-2">
-                            <h1 class="font-black text-3xl text-gray-800"> Bienvenido, Brayan</h1>
-                            <p class="font-normal text-sm text-gray-400">Tu tienes 2 nuevos mensajes y 15 nuevas tareas</p>
-                        </div>
-                        
 
-                        <div>
-                            <a href="#" class="bg-sky-950 text-2xl text-white text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 underline h-8">Mensajes</a> 
+                    <div class="flex flex-nowrap bg-white">
+                        <div class="w-2/5 flex-none p-2">
+                            <div class="text-black text-center bg-white py-2 rounded-lg">
+                                <img src="" alt="">
+                                <p>imagen</p>
+                            </div>
+                        </div>
 
-                            <a href="#" class="bg-indigo-950 text-2xl text-white underline hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 underline h-8">tareas</a>
+                        <div class="w-2/5 flex-none p-2">
+                            <div class="text-center bg-white px-4 py-2 m-2 rounded-lg">
+                                <h1 class="font-black text-3xl text-gray-800"> Bienvenido, Brayan</h1>
+                                <p class="font-normal text-sm text-gray-400">Tu tienes 2 nuevos mensajes y 15 nuevas tareas</p>
+                            </div>
+                        </div>
+
+                        <div class="w-2/5 flex-none p-2">
+                            <div class="text-white text-center bg-white px-4 py-2 m-2 rounded-lg">
+                                <a href="#" class="bg-sky-950 text-2xl text-white text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 underline h-8">Mensajes</a> 
+
+                                <a href="#" class="bg-indigo-950 text-2xl text-white underline hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 underline h-8">tareas</a>
+                            </div>
                         </div>
 
                     </div>
