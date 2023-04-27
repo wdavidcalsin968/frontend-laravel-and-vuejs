38dc5dc28804235fb3dba3e71a21bacac59cc0fa
wdavid <wdavidcalsin968@gmail.com>
2023-04-27 12:39:58 -0500

    feat: add components dahsboard content, navabar and sidebar

---
+++ resources/js/DahboardComponents/DashboardContent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Welcome from "@/Components/Welcome.vue";
+</script>
+
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div
+                class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+            >
+                <Welcome />
+            </div>
+        </div>
+    </div>
+</template>
